6b427e8de5177b83fd71bc4f7af6d58f268bcbf2
s1100616 <s1100616@cadpc28.ccedu.local>
2014-06-12 15:15:08 +0900

    cube

--- Rubik's Cube Demo/src/Cube.java
+++ Rubik's Cube Demo/src/Cube.java
@@ -7,38 +7,78 @@ import com.sun.j3d.utils.universe.*;
 
 public class Cube extends BranchGroup
 {
+	public static final int R =0;
+	public static final int L =1;
+	public static final int U =2;
+	public static final int D =3;
+	public static final int F = 4;
+	public static final int B =5;
 
-	private double mR = 0.5;
-	private double mX = 0;
-	private double mY = 0;
-	private Color3f[] mColors; //F, ,U,R
-	Point3d[] vertices;
-	int[] mIndices = { 0, 1, 2,  0, 2, 3,    4, 5, 1,  4, 1, 0,
-            4, 0, 3,  4, 3, 7,    3, 2, 6,  3, 6, 7,
-            7, 6, 5,  7, 5, 4,    1, 5, 6,  1, 6, 2 };
+	private double mR;
+	private double mX;
+	private double mY;
+	private double mZ;
+	private Color3f[] mColors; //R,L,U,D,F,B
+	private Color3f mBaseColor;
+	Point3d[] mVertices;
+	int[] mIndices = { 3, 2, 6,  3, 6, 7, //R
+								4, 5, 1,  4, 1, 0, //L
+								4, 0, 3,  4, 3, 7, //U
+								1, 5, 6,  1, 6, 2, //D
+								0, 1, 2,  0, 2, 3, //F
+								7, 6, 5,  7, 5, 4 //B
+							};
 	int[] colorIndices = { 0, 0, 0,  0, 0, 0,    1, 1, 1,  1, 1, 1,
             2, 2, 2,  2, 2, 2,    3, 3, 3,  3, 3, 3,
             4, 4, 4,  4, 4, 4,    5, 5, 5,  5, 5, 5 };
 
-	public Cube()
+	public Cube(double x,double y,double z,double r)
 	{
-		mR = 0.5;
-		mX = 0;
-		mY = 0;
-		mColors = new Color3f[6];
-		for(int i=0;i<mColors.length;i++)
+		mR = r;
+		mX = x;
+		mY = y;
+		mZ = z;
+		mBaseColor = new Color3f(Color.BLACK);
+		for (int i=0;i<=mColors.length;i++)
 		{
-			mColors[i] = new Color3f();
+			mColors[i] = new Color3f(Color.BLACK);
 		}
-		vertices = new Point3d[8];
-		vertices[0] =  new Point3d(-mR,mR,mR);	//左上前
-		vertices[1] =  new Point3d(-mR,-mR,mR);	//左下前
-		vertices[2] =  new Point3d(mR,-mR,mR);	//右下前
-		vertices[3] =  new Point3d(mR,mR,mR);		//右上前
-		vertices[4] =  new Point3d(-mR,mR,-mR);	//左上奥
-		vertices[5] =  new Point3d(-mR,-mR,-mR);//左下奥
-		vertices[6] =  new Point3d(mR,-mR,-mR);//右下奥
-		vertices[7] =  new Point3d(mR,mR,-mR);	//右上奥
+		mVertices = new Point3d[8];
+		mVertices[0] =  new Point3d(-mR+mX,mR+mY,mR+mZ);	//左上前
+		mVertices[1] =  new Point3d(-mR+mX,-mR+mY,mR+mZ);	//左下前
+		mVertices[2] =  new Point3d(mR+mX,-mR+mY,mR+mZ);	//右下前
+		mVertices[3] =  new Point3d(mR+mX,mR+mY,mR+mZ);		//右上前
+		mVertices[4] =  new Point3d(-mR+mX,mR+mY,-mR+mZ);	//左上奥
+		mVertices[5] =  new Point3d(-mR+mX,-mR+mY,-mR+mZ);//左下奥
+		mVertices[6] =  new Point3d(mR+mX,-mR+mY,-mR+mZ);//右下奥
+		mVertices[7] =  new Point3d(mR+mX,mR+mY,-mR+mZ);	//右上奥
+
+		generate();
+	}
+
+	private void generate()
+	{
+		 // 立方体を生成
+        IndexedTriangleArray geometry = new IndexedTriangleArray(
+            mVertices.length, GeometryArray.COORDINATES | GeometryArray.COLOR_3, mIndices.length);
+        geometry.setCoordinates(0, mVertices);
+        geometry.setCoordinateIndices(0, mIndices);
+        geometry.setColors(0, mColors);
+        geometry.setColorIndices(0, colorIndices);
+
+        // 回転を設定
+        Shape3D shape = new Shape3D(geometry);
+        TransformGroup trans = new TransformGroup();
+
+        // BranchGroup に登録
+        trans.addChild(shape);
+        addChild(trans);
+	}
+
+	public void setColors(Color3f[] colors)
+	{
+		mColors = colors;
+		generate();
 	}
 
 }
--- Rubik's Cube Demo/src/MainFrame.java
+++ Rubik's Cube Demo/src/MainFrame.java
@@ -1,7 +1,17 @@
+import java.awt.Color;
 import java.awt.GraphicsConfiguration;
+
+import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
 import javax.swing.JFrame;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
 import com.sun.j3d.utils.universe.SimpleUniverse;
+import com.sun.j3d.utils.universe.ViewingPlatform;
 
 public class MainFrame extends JFrame
 {
@@ -26,5 +36,29 @@ public class MainFrame extends JFrame
 		// SimpleUniverseを生成
 		SimpleUniverse universe = new SimpleUniverse(canvas);
 		universe.getViewingPlatform().setNominalViewingTransform();
+
+		//視点についてのハードウェア情報を取得。
+        ViewingPlatform vp = universe.getViewingPlatform();
+
+        //視点のための座標変換クラスを用意
+        TransformGroup Camera = vp.getViewPlatformTransform();
+
+        Transform3D cameraTrans = new Transform3D();
+        cameraTrans.lookAt(new Point3d(5,5,10),new Point3d(0,0,0), new Vector3d(0,1,0));
+        cameraTrans.invert();
+        Camera.setTransform(cameraTrans);
+
+        Color3f[] colors = new Color3f[6];
+		colors[0] = new Color3f(Color.RED);
+		colors[1] = new Color3f(Color.ORANGE);
+		colors[2] = new Color3f(Color.WHITE);
+		colors[3] = new Color3f(Color.BLUE);
+		colors[4] = new Color3f(Color.GREEN);
+		colors[5] = new Color3f(Color.YELLOW);
+        Cube cube  = new Cube(0, 0, 0, 0.5);
+        universe.addBranchGraph(cube);
+
+        // JFrame を表示
+        setVisible(true);
 	}
 }
